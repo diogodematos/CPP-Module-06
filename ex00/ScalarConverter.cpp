27b74640ab03d7e7969bda0e6e25e022587d3dcb
Diogo de Matos <dcarrilh@c2r4s9.42porto.com>
2024-08-28 12:24:31 +0100

    Almost Done

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: dcarrilh <dcarrilh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 14:33:16 by dcarrilh          #+#    #+#             */
-/*   Updated: 2024/08/27 16:45:08 by dcarrilh         ###   ########.fr       */
+/*   Updated: 2024/08/28 12:09:05 by dcarrilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,26 +48,47 @@ void CheckInput(std::string conv)
     {
         throw ScalarConverter::ErrorInput(); 
     }
-    else if (d == 0 && (unsigned)strlen(res) == 1)
+    else if ((d == 0) && (unsigned)strlen(co) != 1 && res[0] == 'f')
+    {
+        std::cout << "char: Non Displayable" << std::endl;
+        std::cout << "int: " << static_cast<int>(d) << std::endl;
+        std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(d) << std::endl;
+        std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
+    }
+    else if ((d == 0) && (unsigned)strlen(res) == 1)
     {
         std::cout << "char: " << static_cast<char>(res[0]) << std::endl; 
         std::cout << "int: " << static_cast<int>(res[0]) << std::endl;
         std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(res[0]) << std::endl;
         std::cout << "float: " << static_cast<float>(res[0]) << 'f' << std::endl;
     }
-    else if (d < 32 && (unsigned)strlen(res) == 0)
+    else if ((d >= 0 && d < 32) || d == 127)//&& ((unsigned)strlen(res) == 0 || (unsigned)strlen(res) == 1 ))
     {
-        std::cout << "char: " << static_cast<char>(res[0]) << std::endl; 
-        std::cout << "int: " << static_cast<int>(res[0]) << std::endl;
-        std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(res[0]) << std::endl;
-        std::cout << "float: " << static_cast<float>(res[0]) << 'f' << std::endl;
+        std::cout << "char: Non Displayable" << std::endl; 
+        std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
+        std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
+        std::cout << "int: " << static_cast<int>(d) << std::endl;
+    }
+    else if ((d < -2147483648 || d > 2147483647))
+    {
+        std::cout << "char: Impossible" << std::endl; 
+        std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
+        std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
+        std::cout << "int: Impossible" << std::endl;
+    }
+    else if ((d < 0 || d > 127))
+    {
+        std::cout << "char: Impossible" << std::endl; 
+        std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
+        std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
+        std::cout << "int: " << static_cast<int>(d) << std::endl;
     }
     else
     {
-    std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
-    std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
-    std::cout << "int: " << static_cast<int>(d) << std::endl;
-    std::cout << "char: " << static_cast<char>(d) << std::endl;
+        std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
+        std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
+        std::cout << "int: " << static_cast<int>(d) << std::endl;
+        std::cout << "char: " << static_cast<char>(d) << std::endl;
     }
     
     
